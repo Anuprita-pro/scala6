991e15c77637fcd325e24ec4e50d95bb66b60dce
Anuprita Rajput <74887568+Anuprita-pro@users.noreply.github.com>
2022-01-20 20:49:34 +0530

    Add files via upload

---
+++ AriFun.py
@@ -0,0 +1,16 @@
+def add1(a,b):
+	c=0
+	c=a+b
+	print  "Addition of numbers",c;
+
+
+add1(20,30)
+
+#def sub1(a,b):
+#	c=0
+#	c=a-b
+#	print "Subtraction of number",c
+
+#def mul1(a,b):
+#	c=a*b
+#	print "Multiplication of number",c
